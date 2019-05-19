ad782672387963231f0e32da361c4ff7e3f04783
qiujiahong <qiujiahongde@163.com>
2019-05-20 07:00:12 +0800

    添加建造者模式

---
+++ src/main/java/com/nick/design/pattern/creational/builder/Course.java
@@ -0,0 +1,14 @@
+package com.nick.design.pattern.creational.builder;
+
+
+import lombok.Data;
+
+@Data
+public class Course {
+    private String courseName;
+    private String coursePPT;
+    private String courseVideo;
+    private String courseArticle;
+
+    private String courseQA;
+}
